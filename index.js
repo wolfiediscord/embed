a56fb2125dc8ee4b19ae68101a77a81fd347053f
Wolfie <45970663+wolfiediscord@users.noreply.github.com>
2020-04-07 21:01:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-class Embed {
-    constructor(msg) {
-        this.msg = msg;
-        error() { 
-         if(!this.msg) {this.msg = 'Unknown. Please contact the bot author.'};
+exports.success = (msg) => {
+if(!msg) throw '[ERR] You need to have a valid message on the function "embed".'
     let embed = {
-      description: `<:wolfx:695361329803821086> ${this.msg}`,
-      color: '#ff0000'
-    }
-    return embed;
-        }
-        success() {
-     if(!this.msg) throw '[ERR] You need to have a valid message on the function "embed".'
-    let embed = {
-     description: `<:wolfcheckmark:695361282219442286> ${this.msg}`,
+     description: `<:wolfcheckmark:695361282219442286> ${msg}`,
      color: '#2fa811'
    }
    return embed;
-    }
 }
-module.exports = Embed;
+exports.error = (msg) => {
+    if(!msg) {this.msg = 'Unknown. Please contact the bot author.'};
+    let embed = {
+      description: `<:wolfx:695361329803821086> ${msg}`,
+      color: '#ff0000'
+    }
+    return embed;
+}
