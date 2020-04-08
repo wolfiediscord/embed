121c9b9667e850798808302a2e7eee27c95cb71c
Wolfie <45970663+wolfiediscord@users.noreply.github.com>
2020-04-07 19:22:39 -0500

    Fixed an issue where msg wouldn't be defined.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ class Embed {
         this.msg = msg;
     }
 }
-exports.error = () => {
+exports.error = (msg) => {
     if(!msg) {msg = 'Unknown. Please contact the bot author.'};
     let embed = {
       description: `<:wolfx:695361329803821086> ${msg}`,
@@ -11,7 +11,7 @@ exports.error = () => {
     }
     return embed;
 }
-exports.success = () => {
+exports.success = (msg) => {
     if(!msg) throw '[ERR] You need to have a valid message on the function "embed".'
     let embed = {
      description: `<:wolfcheckmark:695361282219442286> ${msg}`,
