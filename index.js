40ae8ca890e6cad38ceb450d2d1fc76d34e7aaa8
Wolfie <45970663+wolfiediscord@users.noreply.github.com>
2020-04-07 22:03:03 -0500

    Update index.js
    Added:
    ```
    embed.info('message');
    embed.warn('message');
    ```

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ if(!msg) throw '[ERR] You need to have a valid message on the function "embed".'
    }
    return embed;
 }
+exports.warn = (msg) => {
+if(!msg) throw '[ERR] You need to have a valid message on the function "embed".'
+    let embed = {
+     description: `<:wolfwarn:697276847422963733> ${msg}`,
+     color: '#fced1c'
+    }
+    return embed;
+}
+exports.info = (msg) => {
+if(!msg) throw '[ERR] You need to have a valid message on the function "embed".'
+    let embed = {
+    description: `<:wolfinfo:697279215174680589>  ${msg}`,
+    color: '#3c8de7'
+    }
+    return embed;
+}
 exports.error = (msg) => {
     if(!msg) {this.msg = 'Unknown. Please contact the bot author.'};
     let embed = {
