a80f9c544bb4511f5572189e09c55f6cab0ad6c7
Wolfie <45970663+wolfiediscord@users.noreply.github.com>
2020-04-07 20:37:43 -0500

    Fixes not a function error

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-module.exports = (msg) => {
-    this.error = function () { 
-         if(!msg) {msg = 'Unknown. Please contact the bot author.'};
+class Embed {
+    constructor(msg) {
+        this.msg = msg;
+    }
+    error() {
+     if(!msg) {msg = 'Unknown. Please contact the bot author.'};
     let embed = {
       description: `<:wolfx:695361329803821086> ${msg}`,
       color: '#ff0000'
     }
     return embed;
-    };
-    this.success = function () {
-    if(!msg) throw '[ERR] You need to have a valid message on the function "embed".'
+    }
+    success() {
+     if(!msg) throw '[ERR] You need to have a valid message on the function "embed".'
     let embed = {
      description: `<:wolfcheckmark:695361282219442286> ${msg}`,
      color: '#2fa811'
    }
    return embed;
     }
- }
+}
+module.exports = Embed;
